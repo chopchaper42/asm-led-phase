b17462250127516f9f57f5fa04a79c6a3b237c9f
chopchaper42 <vorongri@fel.cvut.cz>
2024-10-05 12:10:08 +0200

    both modes work, but timing is slightly off

--- main.s
+++ main.s
@@ -22,49 +22,67 @@ _start: @ jako main v c
     
 loop:
     ldr r0, =GPIOA_IDR  @ Read input of port A
-    ldr r1, [r0]       @ load the value stored on address from r0 to r1. r1 = 0x40010808
+    ldr r1, [r0]        @ load the value stored on address from r0 to r1. r1 = 0x40010808
     tst r1, #0x1        @ Test if 1st pin is HIGH
-    beq loop1
-    bl turn_led_on
+    
+    beq loop1          @ if button isn't pressed, go to loop1
     mov r0, #1         @ put 1 to R0 to use in delay
     bl delay           @ delay to avoid switch bouncing
+
+    bl green_led_off
+    bl blue_led_on
+    
+    mov r0, #1         @ put 1 to R0 to use in delay
+    bl delay           @ delay to avoid switch bouncing
+
+    bl green_led_on
+    bl blue_led_off
+    mov r0, #1         @ put 1 to R0 to use in delay
+    bl delay           @ delay to avoid switch bouncing
+
     b loop
 
 loop1:
-    bl turn_led_off
+    bl green_led_on
+    bl blue_led_on
+    mov r0, #1         @ put 1 to R0 to use in delay
+    bl delay           @ delay to avoid switch bouncing
+    bl green_led_off
+    bl blue_led_off
     mov r0, #1         @ put 1 to R0 to use in delay
     bl delay           @ delay to avoid switch bouncing
 b loop
 
-blink_in_phase:
-
-
 
 blue_led_on:
     ldr r0, =GPIOC_ODR @ load GPIOC_ODR address to R0
-    ;ldr r1, =0x100     @ set 8th pin HIGH
-    mov r1, r0
-    orr r1, =0x100
+    ldr r1, [r0]         @ move r0 to r1
+    orr r1, #0x100     @ set 8th pin to 1
     str r1, [r0]       @ store R1 to [R0] - GPIOC_ODR
-
-bx lr
-
-blue_led_off:
-    ldr r0, =GPIOC_ODR  @ load GPIOC_ODR address to R0
-    bic r1, #0x100        @ put 0 to r1
-    str r1, [r0]        @ store 0 from r1 to GPIOC_ODR
 
 bx lr
 
 green_led_on:
     ldr r0, =GPIOC_ODR @ load GPIOC_ODR address to R0
-    ldr r1, =0x200     @ set 9th pin HIGH
+    ldr r1, [r0]         @ move r0 to r1
+    orr r1, #0x200     @ set 8th pin to 1
     str r1, [r0]       @ store R1 to [R0] - GPIOC_ODR
 
 bx lr
 
+
+blue_led_off:
+    ldr r0, =GPIOC_ODR  @ load GPIOC_ODR address to R0
+    ldr r1, [r0]
+    bic r1, #0x100        @ put 0 to r1
+    str r1, [r0]        @ store 0 from r1 to GPIOC_ODR
+
+bx lr
+
+
 green_led_off:
     ldr r0, =GPIOC_ODR  @ load GPIOC_ODR address to R0
+    ldr r1, [r0]
     bic r1, #0x200        @ put 0 to r1
     str r1, [r0]        @ store 0 from r1 to GPIOC_ODR
 
@@ -83,7 +101,7 @@ setup_gpio:
     ldr r0, =GPIOC_CRH @ CRH - control register high
     ldr r1, [r0]       @ write value of r0 to r1
     bic r1, #0xFF      @ clear first 8 bits (pins 8 and 9)
-    orr r1, #0x1       @ set first 8 bits to 0000 0001 (push-pull OUT 10MHz) PC8
+    orr r1, #0x11       @ set first 8 bits to 0000 0001 (push-pull OUT 10MHz) PC8
     str r1, [r0]       @ store R1 to GPIOC_CRH
 
     ldr r0, =GPIOA_CRL   @ load GPIOA_CRL to R0
@@ -96,7 +114,7 @@ bx lr
 
 delay:
 
-    ldr r1, =0x20000
+    ldr r1, =0x100000
 delay_second:
     subs r1, #1
     bne delay_second
