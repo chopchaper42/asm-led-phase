8dc9161e0188752b3211e1048a637bbc7da3636f
chopchaper42 <vorongri@fel.cvut.cz>
2024-10-19 16:48:56 +0200

    works, but timings are strange

--- main.s
+++ main.s
@@ -33,55 +33,43 @@ loop:
     ldr r1, [r0]        @ load the value stored on address from r0 to r1. r1 = 0x40010808
     tst r1, #1          @ Test if 1st pin is HIGH
 
-    ldr r0, =0x384E6     @ 0,1sec
+    ittee ne            @ if the button is pressed
+    movne r5, #0        @ reset the timer to go straight to on_timer_zero
+    movne r7, #1        @ set the BUTTON_PRESSED flag
+    moveq r7, #0        @ else clear the BUTTON_PRESSED flag and
+    beq timer           @ go straight to timer (no need to debounce)
+
 debounce:
+    ldr r0, =0x384E6     @ ~0,1sec
+debounce_internal:
     subs r0, #1
-    bne debounce
+    bne debounce_internal
 
-@@@@@@@@@@@@@ TEST @@@@@@@@@@@@@@@@@@@@@@@
-
-    cmp r5, #0          @ compare TIMER with 0
-    beq test_on_zero    @ if TIMER == 0, jump to on_timer_zero
-
-    subs r5, #1          @ else subtract 1 from TIMER
-    b loop              @ and jump to loop
-
-test_on_zero:
-    @ldr r5, =0x384E6   @ set TIMER for 0,5 sec
-
-    bl toggle_green
-
-b loop
-
-
-
-@@@@@@@@@@@@@ /TEST @@@@@@@@@@@@@@@@@@@@@@
-
-    @ after debounce handling
+timer:
     cmp r5, #0          @ compare TIMER with 0
     beq on_timer_zero   @ if TIMER == 0, jump to on_timer_zero
 
     subs r5, #1          @ else subtract 1 from TIMER
-    b loop              @ and jump to loop
+    b loop               @ and jump to loop
 
 on_timer_zero:
-    ldr r5, =0x11987E   @ set TIMER for 0,5 sec
-
-    cmp r7, #0          @ check if the BUTTON_PRESSED flag is 0
-    beq out_of_phase
-
-    @ in phase
-    cmp r6, #0          @ check if PHASE flag is zero
+    ldr r5, =0x8CC3F   @ set TIMER for ~0,5 sec
     eor r6, #1          @ toggle the PHASE flag
 
+    cmp r7, #0          @ check if the BUTTON_PRESSED flag is 0
+    beq out_of_phase    @ if button isn't pressed, goto out_of_phase
+
+    @ otherwise in phase
+    cmp r6, #0          @ check if PHASE flag is zero
     beq turn_on         @ if it is zero, turn the leds on
-    
-    bl turn_leds_off    @ turn leds off
+    bl blue_led_off    @ otherwise turn leds off
+    bl green_led_off
 
 b loop
 
 turn_on:
-    bl turn_leds_on     @ turn the leds on
+    bl blue_led_on
+    bl green_led_on
 
 b loop
 
@@ -89,15 +77,14 @@ out_of_phase:
     bl turn_leds_off
     
     cmp r6, #0      @ check the PHASE flag
-    eor r6, #1      @ toggle the PHASE flag
-
     beq green_phase @ turn on the green
-
     bl blue_led_on
+    bl green_led_off
 
 b loop
 
 green_phase:
+    bl blue_led_off
     bl green_led_on
 
 b loop
@@ -139,6 +126,15 @@ toggle_green:
     ldr r0, =GPIOC_ODR @ load GPIOC_ODR address to R0
     ldr r1, [r0]         @ move r0 to r1
     eor r1, #0x200     @ set 9th pin to 1
+    str r1, [r0]       @ store R1 to [R0] - GPIOC_ODR
+
+bx lr
+
+@ make sure that leds have different state if you want them to be out of phase
+toggle_leds:
+    ldr r0, =GPIOC_ODR @ load GPIOC_ODR address to R0
+    ldr r1, [r0]         @ move r0 to r1
+    eor r1, #0x300     @ toggle 8th and 9th pins
     str r1, [r0]       @ store R1 to [R0] - GPIOC_ODR
 
 bx lr
